7dbe3884c7d4306ddf7dcd0bf58ad592334219e1
rickydo <g3tdo@yahoo.com>
2015-04-13 20:45:08 -0500

    added curl command for create

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ Constraints:
 cacheable, uniform interface, layered system, code on demand, stateless, client-server relationship
 
 use curl <uri>.json in terminal to test api
+
+create:
+curl \
+-i \
+-H "Accept: application/json" \
+-H "Content-type: application/json" (this says json is being sent to server and not one whole string")
+-X POST \
+-d '{"title":"The will go here"}' http://localhost:3000/api/todo_lists
+
+where: 
+-i tells it to include the header in the output
+-H sends it a header to accept json
+-X to use HTTP request 
+-d data
--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -1,4 +1,6 @@
 class Api::TodoListsController < ApplicationController
+	skip_before_filter :verify_authenticity_token
+	# disable csrf for curl request otherwise it breaks 
 	def index
     	render json: TodoList.all
   	end
@@ -6,5 +8,22 @@ class Api::TodoListsController < ApplicationController
   	def show
   		list = TodoList.find(params[:id])
   		render json: list
+  	end
+
+  	def create
+  		list = TodoList.new(list_params)
+  		if list.save
+  			head 200
+  			# success
+  		else 
+  			head 500
+  			# fail
+  		end
+  	end
+
+  	private 
+
+  	def list_params
+  		params.require("todo_list").permit("title")
   	end
 end
